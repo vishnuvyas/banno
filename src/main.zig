2092a0d78881cd00726e695fc7e34756aae25e84
Vishnu Vyas <vishnuvyas@quantifiedmind.net>
2025-09-26 07:09:04 -0700

    updated with build.zig

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,22 @@
 const std = @import("std");
 
 fn manageArgs(alloc: std.mem.Allocator) void {
-    const args = std.process.argsAlloc(alloc);
+    const args = std.process.argsAlloc(alloc) catch |err| {
+        std.debug.print("Error encounterd while processing args {any} \n", .{err});
+        return;
+    };
+    defer std.process.argsFree(alloc, args); 
+    for (args) |arg| {
+        std.debug.print("Argument {s} \n", .{arg}); 
+    }
 }
 
 pub fn main() !void {
     std.debug.print("Hello, Zig World", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    manageArgs(allocator);
+
 }
