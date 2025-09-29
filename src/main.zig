a1e836e5b5d84a362d3c475f11b18840e0eeabaa
Vishnu Vyas <vishnuvyas@quantifiedmind.net>
2025-09-29 11:22:30 -0700

    feat: added support for tool args

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,38 @@
 const std = @import("std");
 
-fn manageArgs(alloc: std.mem.Allocator) void {
+const Tool = enum { todo, mail };
+
+fn manageArgs(alloc: std.mem.Allocator) !void {
     const args = std.process.argsAlloc(alloc) catch |err| {
         std.debug.print("Error encounterd while processing args {any} \n", .{err});
         return;
     };
-    defer std.process.argsFree(alloc, args); 
-    for (args) |arg| {
-        std.debug.print("Argument {s} \n", .{arg}); 
+    defer std.process.argsFree(alloc, args);
+    if (args.len > 1) {
+        const tool = args[1];
+        const toolEnum = std.meta.stringToEnum(Tool, tool) orelse {
+            return error.InvalidToolName;
+        };
+        switch (toolEnum) {
+            Tool.todo => {
+                std.debug.print("Provided todo tool\n", .{});
+                const toolArgs = args[2..];
+                for (toolArgs) |toolArg| {
+                    std.debug.print("Provided tool arg {s}\n", .{toolArg});
+                }
+            },
+            else => {
+                std.debug.print("UnImplemented tool {any}", .{toolEnum});
+            },
+        }
     }
 }
 
 pub fn main() !void {
-    std.debug.print("Hello, Zig World", .{});
+    std.debug.print("Hello, Zig World\n", .{});
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
-    manageArgs(allocator);
-
+    try manageArgs(allocator);
 }
